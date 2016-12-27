8d7115a227e8387075255db29c1d9aa91be73f37
liuxx94 <963187657@qq.com>
2016-12-27 14:19:39 +0800

    emputy->empty

--- src/main/java/com/moxing/ssm/model/ResponseObj.java
+++ src/main/java/com/moxing/ssm/model/ResponseObj.java
@@ -1,8 +1,8 @@
 package com.moxing.ssm.model;
 
 public class ResponseObj<T> {
-	public final static int OK = 1, FAILED = 0, EMPUTY = -1;
-	public final static String OK_STR = "成功", FAILED_STR = "失败", EMPUTY_STR = "数据为空";
+	public final static int OK = 1, FAILED = 0, EMPTY = -1;
+	public final static String OK_STR = "成功", FAILED_STR = "失败", EMPTY_STR = "数据为空";
 
 	private int code; // 状态码,0成功;1空数据;-1请求失败
 	private String msg;
