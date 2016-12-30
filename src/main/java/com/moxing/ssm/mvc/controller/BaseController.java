ff3c56bbe32c02c92b4e16f80a3aee6b613bef01
matf5 <419194613@qq.com>
2016-12-30 14:35:50 +0800

    上传图片

---
+++ src/main/java/com/moxing/ssm/mvc/controller/BaseController.java
@@ -0,0 +1,49 @@
+package com.moxing.ssm.mvc.controller;
+
+import com.moxing.ssm.model.ResponseObj;
+import com.moxing.ssm.model.User;
+import com.moxing.ssm.utils.GsonUtils;
+import com.moxing.ssm.utils.MyCos;
+import com.qcloud.cos.request.UploadFileRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by mtf on 2016/12/29.
+ */
+@Controller
+@RequestMapping("/baseAction")
+public class BaseController {
+    private ResponseObj responseObj;
+    @RequestMapping(value = "/uploadHeadImg"
+            , method = RequestMethod.POST
+            , produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public String uploadHeadImg(HttpServletRequest request) throws IOException {
+        String img = request.getParameter("img");
+        byte[] butter = new BASE64Decoder().decodeBuffer(img);
+       // InputStream inputStream = new ByteArrayInputStream(new BASE64Decoder().decodeBuffer(img));
+            responseObj = new ResponseObj<User>();
+
+            UploadFileRequest uploadFileRequest = new UploadFileRequest(MyCos.getBucketName(),"/sample_file.jpg",butter);
+            String uploadFileRet = MyCos.getClient().uploadFile(uploadFileRequest);
+            System.out.println(uploadFileRet);
+            responseObj.setCode(ResponseObj.EMPTY);
+            responseObj.setMsg(uploadFileRet);
+
+
+        return new GsonUtils().toJson(responseObj);
+    }
+
+}
